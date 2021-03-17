ea4ebae1b335d2bec3bd1115bf9a942999f2996d
amrabdalfatah <amr.abdalfatah.mohamed@gmail.com>
2021-03-17 22:01:27 +0200

    Finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,7 +25,53 @@ class DiceeApp extends StatelessWidget{
         title: Text('Dicee App'),
         backgroundColor: Colors.red,
       ),
-      body: Container(),
+      body: DicePage(),
     );
   }
 }
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftNumber = 1;
+  int rightNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('assets/images/dice$leftNumber.png'),
+              onPressed: () {
+                setState(() {
+                  leftNumber = Random().nextInt(7);
+                  while(leftNumber == 0){
+                    leftNumber = Random().nextInt(7);
+                  }
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('assets/images/dice$rightNumber.png'),
+              onPressed: () {
+                setState(() {
+                  rightNumber = Random().nextInt(7);
+                  while(rightNumber == 0){
+                    rightNumber = Random().nextInt(7);
+                  }
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
